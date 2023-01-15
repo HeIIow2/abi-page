cd192e187204a57dbfa3cb91a5c98a04298d83a5
Hellow <Lars.Noack@outlook.de>
2023-01-15 18:54:38 +0100

    updated contents

--- src/assets/update.sh
+++ src/assets/update.sh
@@ -22,7 +22,7 @@ pandoc --pdf-engine=xelatex -V geometry:margin=1in -o Deutsch/expressionismus.pd
 
 cp ./Deutsch/expressionismus.md /media/lars/Volume/Projekts/school/abi_page/src/assets/deutsch/expressionismus.md
 cp ./Deutsch/expressionismus.pdf /media/lars/Volume/Projekts/school/abi_page/src/assets/deutsch/expressionismus.pdf
-cp -a ./*/assets/ /media/lars/Volume/Projekts/school/abi_page/src/assets/
+cp -a ./*/assets/ /media/lars/Volume/Projekts/school/abi_page/src/
 
 
 cd /media/lars/Volume/Projekts/school/abi_page
